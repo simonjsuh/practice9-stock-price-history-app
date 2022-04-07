cd504010ab96f558203480abfb418007d32b9010
simonjsuh <simon.j.suh@gmail.com>
2022-04-07 10:16:54 -0700

    stock dates and array updating
    stock dates and array updating but without refs

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="lineChart">
-    <div class="chartContainer">
-      <button @click="createStockPriceHistoryChartInSpecifiedDateHistoryRange()">1 day</button>
-      <button @click="createStockPriceHistoryChartInSpecifiedDateHistoryRange()">1 week</button>
-      <button @click="createStockPriceHistoryChartInSpecifiedDateHistoryRange()">1 month</button>
-      <button @click="createStockPriceHistoryChartInSpecifiedDateHistoryRange()">1 year</button>
-      <button @click="createStockPriceHistoryChartInSpecifiedDateHistoryRange()">5 years</button>
-      <button @click="createStockPriceHistoryChartInSpecifiedDateHistoryRange(0)">MAX</button>
-      <canvas id="myChart" width="400px" height="400px"></canvas>
+    <div class="chartAndButtonsContainer">
+      <button @click="createStockPriceHistoryChartInSpecifiedDateHistoryRange(86400)">1 day</button>
+      <button @click="createStockPriceHistoryChartInSpecifiedDateHistoryRange(604800)">1 week</button>
+      <button @click="createStockPriceHistoryChartInSpecifiedDateHistoryRange(2630000)">1 month</button>
+      <button @click="createStockPriceHistoryChartInSpecifiedDateHistoryRange(31536000)">1 year</button>
+      <button @click="createStockPriceHistoryChartInSpecifiedDateHistoryRange(157680000 )">5 years</button>
+      <button @click="createStockPriceHistoryChartInSpecifiedDateHistoryRange(10000000000000000)">MAX</button>
+
+      <div id="chartContainer">
+        <canvas id="myChart" width="400px" height="400px"></canvas>
+      </div>
     </div>
   </div>
 </template>
@@ -18,7 +21,11 @@ import axios from 'axios'
 
 let stockMarketHistory = [];
 let stockMarketHistoryDates = [];
+let stockMarketHistoryEpochDates = [];
 let stockMarketHistoryPrices = [];
+
+let myChart;
+myChart;
 
 let updateStockPriceHistoryChart = () => {
   // beginning of line chart code //
@@ -42,6 +49,10 @@ let updateStockPriceHistoryChart = () => {
     }]
   };
 
+  // if (typeof myChart != 'undefined') {
+  //   myChart.destroy();
+  // }
+
   const myChart = new Chart(ctx, {
     type: 'line',
     data: data,
@@ -50,6 +61,13 @@ let updateStockPriceHistoryChart = () => {
   myChart;
   // end of line chart code //
 }
+
+// let updateStockPriceHistoryChart_IfChartAlreadyExists = () => {
+//   myChart.destroy();
+//   updateStockPriceHistoryChart;
+// }
+
+// updateStockPriceHistoryChart_IfChartAlreadyExists;
 
 export default {
   name: 'LineChart',
@@ -68,7 +86,7 @@ export default {
     let createStockPriceHistoryChartInSpecifiedDateHistoryRange = (dateRange) => {
       axios.get(AlphaVantangeAPI_URL_Link)
         .then(response => {
-          console.log(response);
+          // console.log(response);
           stockMarketHistory = response
 
           for (const property in stockMarketHistory.data["Monthly Adjusted Time Series"]) {
@@ -84,7 +102,7 @@ export default {
             let closingDateYear = property.split('-')[0];
             let closingDateDay = property.split('-')[2];
             let closingDateProperlyFormatted = `${closingDateMonth}/${closingDateDay}/${closingDateYear}`;
-            // let closingDateEpochTime = Date.parse(closingDateProperlyFormatted);
+            let closingDateEpochTime = Date.parse(closingDateProperlyFormatted)/1000;
 
             // console.log(closingDateEpochTime);
             // console.log(closingPrice);
@@ -94,26 +112,156 @@ export default {
             //   stockMarketHistoryPrices.unshift(closingPrice)
             // }
 
-          dateRange;
+            dateRange;
+
+            // stockMarketHistoryDates = [];
+            // stockMarketHistoryPrices = [];
+
+            closingDateProperlyFormatted
+            closingPrice
+            closingDateEpochTime
 
             stockMarketHistoryDates.unshift(closingDateProperlyFormatted)
+            stockMarketHistoryEpochDates.unshift(closingDateEpochTime)
             stockMarketHistoryPrices.unshift(closingPrice)
           }
 
-          console.log(stockMarketHistory)
+          // console.log(stockMarketHistory)
         })
         .then(() => {
           // console.log(stockMarketHistory.data["Monthly Adjusted Time Series"]["2022-03-31"]["4. close"]);
+          
+          console.log(stockMarketHistoryDates.length);
+          console.log(stockMarketHistoryPrices.length);
 
-          // console.log(stockMarketHistoryDates.slice(-3));
-          // console.log(stockMarketHistoryPrices.slice(-3));
+          // stockMarketHistoryDates = [];
+          // stockMarketHistoryPrices = [];
+
+          // find index of stockMarketHistoryDates where the value is less than 5 years ago.
+          
+          console.log(dateRange);
+          let now = Date.now()/1000;
+          console.log('Now: ' + now + '\n5 years ago: ' + (now - dateRange));
+          let dateRangeDate = now - dateRange;
+          console.log('dateRangeDate: ' + dateRangeDate);
+
+          const isLaterDateThanDateRangeDate = (date) => {
+            return date > dateRangeDate;
+          }
+
+          console.log(stockMarketHistoryEpochDates);
+          console.log('dateRangeDate: ' + dateRangeDate);
+          
+          let arrayIndexWhereDateIsLargerThanDateRangeDate = stockMarketHistoryEpochDates.findIndex(isLaterDateThanDateRangeDate);
+
+          console.log(arrayIndexWhereDateIsLargerThanDateRangeDate);
+
+          // update stockMarketHistoryDates and stockMarketHistoryPrices arrays to delete all values before the index indicated by arrayIndexWhereDateIsLargerThanDateRangeDate
+
+          stockMarketHistoryDates.splice(0, arrayIndexWhereDateIsLargerThanDateRangeDate);
+          stockMarketHistoryPrices.splice(0, arrayIndexWhereDateIsLargerThanDateRangeDate);
 
           updateStockPriceHistoryChart();
         });
     }
     // end of stock API code
 
-    createStockPriceHistoryChartInSpecifiedDateHistoryRange(0);
+    createStockPriceHistoryChartInSpecifiedDateHistoryRange(23456345634563463465436);
+  },
+  setup() {
+    // beginning of stock API code
+    let stockSymbol = 'IBM';
+
+    let AlphaVantangeAPI_URL_Link = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=${stockSymbol}&apikey=demo`;
+
+    // let stockDataMonthlyAdjustedClosing = [];
+
+    // let stockMarketHistory = [];
+    // let stockMarketHistoryDates = [];
+    // let stockMarketHistoryPrices = [];
+
+    let createStockPriceHistoryChartInSpecifiedDateHistoryRange = (dateRange) => {
+      axios.get(AlphaVantangeAPI_URL_Link)
+        .then(response => {
+          // console.log(response);
+          stockMarketHistory = response
+
+          for (const property in stockMarketHistory.data["Monthly Adjusted Time Series"]) {
+            // console.log(`${property}: ${stockMarketHistory.data["Monthly Adjusted Time Series"][property]["4. close"]}`);
+            // console.log(`${stockMarketHistory.data["Monthly Adjusted Time Series"][property]["4. close"]}`);
+
+            // let stockDataMonthlyAdjustedClosingDateEpoch = new Date();
+            // console.log(property);
+
+            let closingPrice = stockMarketHistory.data["Monthly Adjusted Time Series"][property]["4. close"];
+
+            let closingDateMonth = property.split('-')[1];
+            let closingDateYear = property.split('-')[0];
+            let closingDateDay = property.split('-')[2];
+            let closingDateProperlyFormatted = `${closingDateMonth}/${closingDateDay}/${closingDateYear}`;
+            let closingDateEpochTime = Date.parse(closingDateProperlyFormatted)/1000;
+
+            // console.log(closingDateEpochTime);
+            // console.log(closingPrice);
+
+            // if (closingDateProperlyFormatted > dateRange) {
+            //   stockMarketHistoryDates.unshift(closingDateProperlyFormatted)
+            //   stockMarketHistoryPrices.unshift(closingPrice)
+            // }
+
+            dateRange;
+
+            // stockMarketHistoryDates = [];
+            // stockMarketHistoryPrices = [];
+
+            closingDateProperlyFormatted
+            closingPrice
+            closingDateEpochTime
+
+            stockMarketHistoryDates.unshift(closingDateProperlyFormatted)
+            stockMarketHistoryEpochDates.unshift(closingDateEpochTime)
+            stockMarketHistoryPrices.unshift(closingPrice)
+          }
+
+          // console.log(stockMarketHistory)
+        })
+        .then(() => {
+          // console.log(stockMarketHistory.data["Monthly Adjusted Time Series"]["2022-03-31"]["4. close"]);
+          
+          console.log(stockMarketHistoryDates.length);
+          console.log(stockMarketHistoryPrices.length);
+
+          // stockMarketHistoryDates = [];
+          // stockMarketHistoryPrices = [];
+
+          // find index of stockMarketHistoryDates where the value is less than 5 years ago.
+          
+          console.log(dateRange);
+          let now = Date.now()/1000;
+          console.log('Now: ' + now + '\n5 years ago: ' + (now - dateRange));
+          let dateRangeDate = now - dateRange;
+          console.log('dateRangeDate: ' + dateRangeDate);
+
+          const isLaterDateThanDateRangeDate = (date) => {
+            return date > dateRangeDate;
+          }
+
+          console.log(stockMarketHistoryEpochDates);
+          console.log('dateRangeDate: ' + dateRangeDate);
+          
+          let arrayIndexWhereDateIsLargerThanDateRangeDate = stockMarketHistoryEpochDates.findIndex(isLaterDateThanDateRangeDate);
+
+          console.log(arrayIndexWhereDateIsLargerThanDateRangeDate);
+
+          // update stockMarketHistoryDates and stockMarketHistoryPrices arrays to delete all values before the index indicated by arrayIndexWhereDateIsLargerThanDateRangeDate
+
+          stockMarketHistoryDates.splice(0, arrayIndexWhereDateIsLargerThanDateRangeDate);
+          stockMarketHistoryPrices.splice(0, arrayIndexWhereDateIsLargerThanDateRangeDate);
+
+          // updateStockPriceHistoryChart();
+        });
+    }
+    // end of stock API code
 
     return {
       createStockPriceHistoryChartInSpecifiedDateHistoryRange,
@@ -124,7 +272,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.chartContainer {
+#chartContainer {
   width: 1000px;
   height: 300px;
 }
